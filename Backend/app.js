74038ef3ca06e8fa952b46c5eadaa5dfb13f4b8a
Amit Sain <amit2976297@gmail.com>
2026-07-24 13:23:33 +0530

    Connection Route Updated

--- Backend/app.js
+++ Backend/app.js
@@ -2,14 +2,15 @@ import express from 'express';
 import fs from 'fs';
 import cors from 'cors'
 import documentRoutes from './routes/documentRoute.js';
+import testRoutes from './routes/test.routes.js';
 
 const app = express();
 
 // Enable CORS so your frontend (e.g., http://localhost:3000) can talk to your backend
 app.use(cors({
-  origin: 'http://localhost:5000', // Change this to your exact frontend URL
-  methods: ['GET', 'POST'],
-  credentials: true
+    origin: 'http://localhost:5173', // Change this to your exact frontend URL
+    methods: ['GET', 'POST'],
+    credentials: true
 }));
 
 // Automatically generate target storage directory if missing locally
@@ -21,6 +22,7 @@ app.use(express.json());
 
 // Bind the router endpoints
 app.use('/api/documents', documentRoutes);
+app.use("/api/test", testRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server executing smoothly on port ${PORT}`));
